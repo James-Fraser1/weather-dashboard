4ecb11315361318f2a7ab5d882aa1da94ca8f6e0
James-Fraser1 <jdfraser2022@gmail.com>
2022-08-01 12:57:47 -0400

    JavaScript function adjusted, html formatting edits

--- assets/script.js
+++ assets/script.js
@@ -17,18 +17,18 @@ setInterval(() => {
     dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
 }, 1000);
 
-
 let weather = {
-    APIKey: "8be59e4e2ee7d03ba73b4748346efb83",
+APIKey: "8be59e4e2ee7d03ba73b4748346efb83",
     fetchWeather: function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.APIKey)
 
         .then((res) => res.json())
         .then((data) => this.displayWeather(data));
+     
     },
     displayWeather: function(data) {
         const { name } = data;
-        const { icon } = data.weather[0];
+        const { icon } = data.weather.id;
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         console.log(name,icon,temp,humidity,speed)
@@ -47,11 +47,5 @@ let weather = {
 document.querySelector(".search-btn").addEventListener("click", function() {
         weather.search();
     });
-
-document.querySelector(".search-bar").addEventListener("keyup", function(event) {
-    if (event.key == "Enter") {
-        weather.search()
-    }
-});
 
 weather.fetchWeather();
